2b5e0306264857e90e4d9e4f903fc469a1b714b4
Carlo R <clo44russo@gmail.com>
2026-03-12 21:09:20 -0400

    Adding Database and more end points/methods for FastAPI

--- FASTAPI Project/main.py
+++ FASTAPI Project/main.py
@@ -4,10 +4,11 @@ Building new project
 
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
-
+items = []
 
 @app.get("/")
 def read_root():
@@ -19,3 +20,20 @@ def say_hello():
     return {"message:" "Hello from FASTAPI"}
 
 
+class Item(BaseModel):
+    name: str
+    price: float
+    in_stock: bool = True
+
+
+
+@app.post("/items")
+def create_item(item: Item):
+    items.append(item.dict())
+    return {"message": "Item added", "item": item}
+
+
+
+@app.get("/items")
+def get_items():
+    return {"items": items}
